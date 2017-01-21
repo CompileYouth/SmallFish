df2a537b320edaecf82ef1b03af94060cdd6e814
CompileYouth <CompileYouth@163.com>
2017-01-21 23:01:27 +0800

    Modify index.ios.js.

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class SmallFish extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Just for test!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
