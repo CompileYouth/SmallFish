55a2ab06d795f724b52177bf094944e83ea62ba2
CompileYouth <CompileYouth@163.com>
2017-02-06 13:59:40 +0800

    We can't use 'div' in RN.

---
+++ src/app/Application.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import {
+  Text,
+  View
+} from 'react-native';
+
+export default class Application extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Blah blah blah</Text>
+      </View>
+    );
+  }
+}
--- src/app/Application.jsx
+++
@@ -1,7 +0,0 @@
-import { Component } from 'react';
-
-export default class Application extends Component {
-  render() {
-    return (<div id='sf-app'>Hello SmallFish</div>);
-  }
-}
