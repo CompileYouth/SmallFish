fb982cdf33e4fc8ee9dc3cdc4829ec9f29ed9bf0
CompileYouth <CompileYouth@163.com>
2017-02-08 15:54:19 +0800

    Modify app overall style.

--- src/app/Application.js
+++ src/app/Application.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 
 import {
   Text,
-  View
+  View,
+  TextInput,
+  StyleSheet
 } from 'react-native';
+
+//import TodoList from '../component/List';
 
 export default class Application extends Component {
   render() {
     return (
-      <View>
-        <Text>Blah blah blah</Text>
+      <View style={styles.app}>
+        <Text>Hello SmallFish</Text>
+        <Text>Hello SmallFish</Text>
+        <Text>Hello SmallFish</Text>
+        <Text>Hello SmallFish</Text>
+        <Text>Hello SmallFish</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  app: {
+    backgroundColor: '#FDD7E4',
+    flex: 1
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import {
+  Dimensions,
+  StyleSheet,
   View
 } from 'react-native';
 
 import Application from './app/Application';
 
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
 export default class SmallFish extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Application />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#123456',
+    width,
+    height,
+    paddingTop: 20
+  }
+});
