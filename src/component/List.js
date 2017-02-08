875aa6605b26fbf14806cf6e2cab71e6e29fc0fe
CompileYouth <CompileYouth@163.com>
2017-02-08 15:22:42 +0800

    Add component.

---
+++ src/component/List.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import {
+  View,
+  ListView
+} from 'react-native';
+
+export default class Application extends Component {
+  render() {
+    return (
+      <ListView>
+        <View />
+        <View />
+      </ListView>
+    );
+  }
+}
