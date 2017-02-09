02714a4751a20846a361effa77e17375f5345cc0
CompileYouth <CompileYouth@163.com>
2017-02-09 12:22:24 +0800

    Add dataSource and renderRow to List.

--- src/component/List.js
+++ src/component/List.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 
 import {
-  ListView,
-  View,
-  Text
+  ListView
 } from 'react-native';
 
-//import Item from './Item';
+import Item from './Item';
 
 export default class List extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.state = {
+      allTodos: ds.cloneWithRows([
+        { text: 'todo1', date: '2017-02-07' },
+        { text: 'todo2', date: '2017-02-07' },
+        { text: 'todo3', date: '2017-02-07' },
+        { text: 'todo4', date: '2017-02-07' },
+        { text: 'todo5', date: '2017-02-07' }
+      ])
+    };
+  }
+
+  _renderTodo(todo) {
+    return (<Item
+      text={todo.text}
+      date={todo.date}
+    />);
+  }
+
   render() {
     return (
-      <View><Text>Hello</Text></View>
+      <ListView
+        dataSource={this.state.allTodos}
+        renderRow={this._renderTodo}
+      />
     );
   }
 }
