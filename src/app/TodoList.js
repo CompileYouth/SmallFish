826fe8e5f7d1b67a44c598ba7298b6abdf989658
CompileYouth <CompileYouth@163.com>
2017-02-09 16:31:33 +0800

    Pull List to create new item.

--- src/app/TodoList.js
+++ src/app/TodoList.js
@@ -1,5 +1,20 @@
+import React from 'react';
+
+import {
+  ListView
+} from 'react-native';
+
 import List from '../component/List';
 
 export default class TodoList extends List {
+  _handleScroll(e) {
+    if (e.nativeEvent.contentOffset.y < -10) {
+      console.log('Now you can create a new item.');
+    }
+  }
 
+  render() {
+    const list = super.render();
+    return React.cloneElement(list, { onScroll: this._handleScroll });
+  }
 }
